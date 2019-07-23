7bc6db87ddea11cdb431cab8caf99da4b4d27f8d
Nicholaus P. Brosowsky <nbrosowsky@gmail.com>
2019-07-23 18:48:52 -0400

    revision in-progress

---
+++ 1st-Submission-01-15-2019/task-relevance-cspc-a51ff3ddedf006918d70484913a01c16b99d6d42/data-analysis/analysis.R
@@ -0,0 +1,207 @@
+
+library(dplyr)
+library(tidyr)
+library(afex)
+library(ggplot2)
+library(cowplot)
+#As of 2018/12/07 apa_print() requires development version of Papaja
+#devtools::install_github("crsh/papaja")
+library(papaja)
+
+
+folderName <- function(){
+  wd<-getwd()
+  wd<-strsplit(wd,"/")
+  wd<-wd[[1]][length(wd[[1]])]
+  
+  return(wd)
+}
+
+
+vjoutNR <- function(x,n) {
+  xm <- mean(x)
+  xsize <- c(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 
+             25, 30, 35, 50, 80)
+  stds <- c(1.3, 1.458, 1.68, 1.841, 1.961, 2.05, 2.12, 2.173, 
+            2.22, 2.246, 2.274, 2.31, 2.326, 2.391, 2.41, 2.4305, 
+            2.45, 2.48, 2.5)
+  stdindex <- length(xsize[xsize <= length(x)])
+  removed <- x[x < xm+sd(x)*stds[stdindex]]
+  removed <- removed[removed > xm - (sd(x)*stds[stdindex])]
+  proportionRemoved <- length(removed)/length(x)
+  finaldata<-c(mean(removed),1-proportionRemoved)
+  return(finaldata[[n]])
+}
+
+
+
+load("data-analysis/raw_data.Rda")
+
+
+# Find subs with < 75% accuracy
+low_acc <- raw_data %>%
+  group_by(Subject,Condition,Task_Relevant_Context,Frequency,Congruency) %>%
+  summarise(meanAccuracy = mean(ACC)) %>%
+  filter(meanAccuracy < .75) %>%
+  .$Subject
+
+N_subjects_removed<-length(unique(low_acc))
+
+## summarise by subject
+RT.DF <- raw_data %>%
+  filter(
+    RT < 3000,
+    RT > 0,
+    ACC == TRUE,
+    Subject%in%low_acc == FALSE
+  ) %>%
+  group_by(Condition,Subject,Order,Phase,Block,Frequency,Task_Relevant_Context,Congruency)%>%
+  summarise(vjoutRT = vjoutNR(RT,1))
+
+## Find percentage of trials removed 
+percent_removed <- raw_data %>%
+  filter(
+    RT < 3000,
+    RT > 0,
+    ACC == TRUE,
+    Subject%in%low_acc == FALSE
+  ) %>%
+  group_by(Condition,Subject,Order,Phase,Frequency,Task_Relevant_Context,Congruency)%>%
+  summarise(percent_removed = vjoutNR(RT,2)) %>%
+  ungroup() %>%
+  summarise(avg_removed = mean(percent_removed)) %>%
+  .$avg_removed
+
+## Analyze RTs for frequency unbiased items
+RT.Analysis <- RT.DF %>%
+  filter(
+    Frequency == "unbiased"
+  ) %>%
+  group_by(Condition,Subject,Task_Relevant_Context,Congruency) %>%
+  summarise(meanRT = mean(vjoutRT))
+
+RT_ANOVA <- aov_car(meanRT ~ Condition*Task_Relevant_Context*Congruency + Error(Subject/Task_Relevant_Context*Congruency), data = RT.Analysis)
+RT_ANOVA <- apa_print(RT_ANOVA, es = "pes")$full_result
+
+## Analyze ACC for frequency unbiased items
+ACC.analysis <- raw_data %>%
+  mutate(error = (1-ACC)*100) %>%
+  filter(
+    Frequency == "unbiased",
+    Subject%in%low_acc == FALSE)  %>%
+  group_by(Subject,Condition,Task_Relevant_Context,Congruency) %>%
+  summarise(error = mean(error))
+
+ACC_ANOVA <- aov_car(error ~ Condition*Task_Relevant_Context*Congruency + Error(Subject/Task_Relevant_Context*Congruency), data = ACC.analysis)
+ACC_ANOVA <- apa_print(ACC_ANOVA, es = "pes")$full_result
+
+####### GRAPH FLANKER EFFECTS #########
+RT.Diff<-RT.DF %>%
+  filter(Frequency == "unbiased") %>%
+  group_by(Condition,Subject,Task_Relevant_Context,Congruency) %>%
+  summarise(
+    RT = mean(vjoutRT)
+  ) %>%
+  spread(Congruency,RT) %>%
+  mutate(Diff = inc - con) %>%
+  select(-con:-inc)%>%
+  group_by(Condition,Task_Relevant_Context) %>%
+  summarise(
+    N = n_distinct(Subject), 
+    Flanker = mean(Diff), 
+    sd = sd(Diff), 
+    SE = sd/sqrt(N)
+  ) 
+
+
+#names(RT.Diff)[names(RT.Diff) == "Task_Relevant_Context"] <- 'Task Relevant\nContext'
+levels(RT.Diff$Task_Relevant_Context) <- c("100% PC", "0% PC")
+levels(RT.Diff$Condition) <- c("Object", "Social", "Social (NR)")
+
+limits <- aes(ymax = Flanker + SE, ymin = Flanker - SE)
+RT.graph <- ggplot(RT.Diff,aes(x=Condition, y=Flanker,fill=Task_Relevant_Context))+
+  geom_bar(stat="identity", position=position_dodge(width=0.9), colour = "black") + 
+  scale_fill_manual(values=c("gray60", "white"))+
+  geom_errorbar(limits, width = .2, position=position_dodge(width=0.9))+
+  coord_cartesian(ylim = c(0,150))+
+  scale_y_continuous(breaks=seq(0, 150, 10), expand = c(0,0))+
+  theme(axis.text=element_text(size=7.5),
+        axis.title=element_text(size=10,face="bold")) +
+  labs(title="")+
+  ylab("Congruency Effect (ms)")+
+  theme(       legend.position=c(1,1),
+               legend.justification = c(1,1),
+               legend.direction = "horizontal",
+               legend.background = element_rect(colour = "black", fill = "white", size=1),
+               legend.box.background = element_rect(colour = "black"),
+               legend.margin = margin(t = .1, r = .10, b = .05, l = .1, unit = "cm"),
+               legend.text = element_text(size = 8),
+               legend.title = element_text(size = 8)
+  ) +
+  guides(fill=guide_legend(title="Task-Relevant Context",title.position = "top"))
+
+
+
+ACC.Diff <- raw_data %>%
+  mutate(error = (1-ACC)*100) %>%
+  filter(
+    Frequency == "unbiased",
+    Subject%in%low_acc == FALSE)  %>%
+  group_by(Subject,Condition,Task_Relevant_Context,Congruency) %>%
+  summarise(error = mean(error)) %>%
+  group_by(Condition,Subject,Task_Relevant_Context,Congruency) %>%
+  summarise(
+    error = mean(error)
+  ) %>%
+  spread(Congruency,error) %>%
+  mutate(Diff = inc - con) %>%
+  select(-con:-inc)%>%
+  group_by(Condition,Task_Relevant_Context) %>%
+  summarise(
+    N = n_distinct(Subject), 
+    Flanker = mean(Diff), 
+    sd = sd(Diff), 
+    SE = sd/sqrt(N)
+  ) 
+
+
+levels(ACC.Diff$Task_Relevant_Context) <- c("100% PC", "0% PC")
+levels(ACC.Diff$Condition) <- c("Object", "Social", "Social (NR)")
+
+limits <- aes(ymax = Flanker + SE, ymin = Flanker - SE)
+ACC.graph <- ggplot(ACC.Diff,aes(x=Condition, y=Flanker,fill=Task_Relevant_Context))+
+  geom_bar(stat="identity", position=position_dodge(width=0.9), colour = "black") + 
+  scale_fill_manual(values=c("gray60", "white"))+
+  geom_errorbar(limits, width = .2, position=position_dodge(width=0.9))+
+  coord_cartesian(ylim = c(0,10))+
+  scale_y_continuous(breaks=seq(0, 10, 1), expand = c(0,0))+
+  theme(axis.text=element_text(size=7.5),
+        axis.title=element_text(size=10,face="bold")) +
+  labs(title="")+
+  ylab("Congruency Effect (% Error)")+
+  theme(       legend.position=c(1,1),
+               legend.justification = c(1,1),
+               legend.direction = "horizontal",
+               legend.background = element_rect(colour = "black", fill = "white", size=1),
+               legend.box.background = element_rect(colour = "black"),
+               legend.margin = margin(t = .1, r = .10, b = .05, l = .1, unit = "cm"),
+               legend.text = element_text(size = 8),
+               legend.title = element_text(size = 8)
+  ) +
+  guides(fill=guide_legend(title="Task-Relevant Context",title.position = "top"))
+
+#ACC.graph
+
+########## ARRANGE #########
+figure2<-plot_grid(RT.graph,NULL,ACC.graph, 
+                   nrow = 1, 
+                   rel_widths = c(1, 0.05, 1),
+                   labels = c("A", "", "B"))
+
+#figure2
+
+#ggsave("figure2.pdf", device = "pdf", dpi = 600,
+#      width = 6.875, height = 4, units = "in") 
+
+#clean up environment
+#rm(list=c("ACC.analysis","aov.out","raw_data","RT.Analysis","RT.DF","low_acc","vjoutNR", "RT.Diff"))
---
+++ 1st-Submission-01-15-2019/task-relevance-cspc-a51ff3ddedf006918d70484913a01c16b99d6d42/data-analysis/table1.R
@@ -0,0 +1,214 @@
+library(dplyr)
+library(tidyr)
+library(afex)
+library(ggplot2)
+library(cowplot)
+#As of 2018/12/07 apa_print() requires development version of Papaja
+#devtools::install_github("crsh/papaja")
+library(papaja)
+
+
+folderName <- function(){
+  wd<-getwd()
+  wd<-strsplit(wd,"/")
+  wd<-wd[[1]][length(wd[[1]])]
+  
+  return(wd)
+}
+
+
+vjoutNR <- function(x,n) {
+  xm <- mean(x)
+  xsize <- c(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 
+             25, 30, 35, 50, 80)
+  stds <- c(1.3, 1.458, 1.68, 1.841, 1.961, 2.05, 2.12, 2.173, 
+            2.22, 2.246, 2.274, 2.31, 2.326, 2.391, 2.41, 2.4305, 
+            2.45, 2.48, 2.5)
+  stdindex <- length(xsize[xsize <= length(x)])
+  removed <- x[x < xm+sd(x)*stds[stdindex]]
+  removed <- removed[removed > xm - (sd(x)*stds[stdindex])]
+  proportionRemoved <- length(removed)/length(x)
+  finaldata<-c(mean(removed),1-proportionRemoved)
+  return(finaldata[[n]])
+}
+
+
+## manuscript.Rmd is called from parent
+load("data-analysis/raw_data.Rda")
+
+
+
+
+# Find subs with < 75% accuracy
+low_acc <- raw_data %>%
+  group_by(Subject,Condition,Task_Relevant_Context,Frequency,Congruency) %>%
+  summarise(meanAccuracy = mean(ACC)) %>%
+  filter(meanAccuracy < .75) %>%
+  .$Subject
+
+########## Create table
+
+
+## summarise by subject
+RT.DF <- raw_data %>%
+  filter(
+    RT < 3000,
+    RT > 0,
+    ACC == TRUE,
+    Subject%in%low_acc == FALSE
+  ) %>%
+  group_by(Condition,Subject,Frequency,Order,Phase,Block,Frequency,Task_Relevant_Context,Congruency)%>%
+  summarise(vjoutRT = vjoutNR(RT,1)) %>%
+  group_by(Condition,Subject,Frequency,Task_Relevant_Context,Congruency) %>%
+  summarise(meanRT = mean(vjoutRT)) %>%
+  group_by(Condition,Frequency,Task_Relevant_Context,Congruency) %>%
+  summarise(N = n_distinct(Subject),
+            RT = mean(meanRT),
+            std = sd(meanRT),
+            se = std/sqrt(N))
+
+RT.DF$PC <- NA
+RT.DF[RT.DF$Frequency == "unbiased",]$PC <- "50%"
+RT.DF[RT.DF$Frequency == "biased" & RT.DF$Congruency == "inc",]$PC <- "0%"
+RT.DF[RT.DF$Frequency == "biased" & RT.DF$Congruency == "con",]$PC <- "100%"
+names(RT.DF)[names(RT.DF)=="RT"]<-"oldRT"
+RT.DF$PC<-as.factor(RT.DF$PC)
+
+
+RT.table <- RT.DF %>%
+  mutate(RT = paste(as.character(round(oldRT))," (", as.character(round(se)), ")", sep="")) %>%
+  ungroup() %>%
+  select(Condition,PC,Task_Relevant_Context,Congruency,RT)
+
+
+## summarise by subject
+ACC.DF <- raw_data %>%
+  filter(
+    Subject%in%low_acc == FALSE
+  ) %>%
+  group_by(Condition,Subject,Frequency,Order,Phase,Block,Frequency,Task_Relevant_Context,Congruency)%>%
+  summarise(ER = (1-mean(ACC))*100)  %>%
+  group_by(Condition,Subject,Frequency,Task_Relevant_Context,Congruency) %>%
+  summarise(meanER = mean(ER)) %>%
+  group_by(Condition,Frequency,Task_Relevant_Context,Congruency) %>%
+  summarise(N = n_distinct(Subject),
+            ER = mean(meanER),
+            std = sd(meanER),
+            se = std/sqrt(N))
+
+ACC.DF$PC <- NA
+ACC.DF[ACC.DF$Frequency == "unbiased",]$PC <- "50%"
+ACC.DF[ACC.DF$Frequency == "biased" & ACC.DF$Congruency == "inc",]$PC <- "0%"
+ACC.DF[ACC.DF$Frequency == "biased" & ACC.DF$Congruency == "con",]$PC <- "100%"
+names(ACC.DF)[names(ACC.DF)=="ER"]<-"oldER"
+ACC.DF$PC<-as.factor(ACC.DF$PC)
+
+ACC.table <- ACC.DF %>%
+  mutate(ER = paste(as.character(round(oldER,digits=2))," (", as.character(round(se, digits=2)), ")", sep="")) %>%
+  ungroup() %>%
+  select(Condition,PC,Task_Relevant_Context,Congruency,ER)
+
+table<-merge(RT.table,ACC.table)
+names(table)[names(table)=="Task_Relevant_Context"]<-"Task Relevant Context"
+levels(table$`Task Relevant Context`)<-c("Task Relevant Context: 100% PC", "Task Relevant Context: 0% PC")
+levels(table$Condition)<-c("Object","Social","Social (NR)")
+table$PC <- factor(table$PC, levels = c("0%", "50%", "100%"))
+
+
+
+## need to reshape because kable cannot do nested tables automatically
+t100<-(table[table$`Task Relevant Context` == "Task Relevant Context: 100% PC",])
+t100<-t100[,-3]
+t100<-rbind(t100,
+            c("Object","0%","con","-","-","-","-"),
+            c("Social","0%","con","-","-","-","-"),
+            c("Social (NR)","0%","con","-","-","-","-"),
+            c("Object","100%","inc","-","-","-","-"),
+            c("Social","100%","inc","-","-","-","-"),
+            c("Social (NR)","100%","inc","-","-","-","-")
+)
+
+t100<-cbind(
+  t100[t100$Congruency == "con",] %>%
+    arrange(Condition,PC), 
+  t100[t100$Congruency == "inc",] %>%
+    arrange(Condition,PC))
+
+t100<-t100[c(1,2,4,5,9,10)]
+colnames(t100)<-c("Condition","PC","RT","ER","RT","ER")
+
+
+t0<-cbind(table[table$`Task Relevant Context` == "Task Relevant Context: 0% PC",])
+t0<-t0[,-3]
+t0<-rbind(t0,
+          c("Object","0%","con","-","-","-","-"),
+          c("Social","0%","con","-","-","-","-"),
+          c("Social (NR)","0%","con","-","-","-","-"),
+          c("Object","100%","inc","-","-","-","-"),
+          c("Social","100%","inc","-","-","-","-"),
+          c("Social (NR)","100%","inc","-","-","-","-")
+)
+t0<-cbind(
+  t0[t0$Congruency == "con",] %>%
+    arrange(Condition,PC), 
+  t0[t0$Congruency == "inc",] %>%
+    arrange(Condition,PC))
+t0<-t0[,c(1,2,4,5,9,10)]
+colnames(t0)<-c("Condition","PC","RT","ER","RT","ER")
+
+
+table<-rbind(t100,t0)
+colnames(table)<-c("Condition","PC","RT","ER","RT","ER")
+
+table$TRC<-as.factor(c(rep("100% PC", 9), rep("0% PC", 9)))
+table<-table[c(7,1:6)]
+colnames(table)<-c("Task-Relevant Context","Condition","PC","RT","ER","RT","ER")
+
+forTable<-as.matrix(table[,4:7])
+
+TR_Table<-paste(
+    "\\begin{table}[htbp]",
+    "\\caption{Reaction times and error rates from Experiment 1.}",
+    "\\label{TR_table}",
+    "\\centering",
+    "\\begin{tabular}{lllcccc}",
+    "\\toprule",
+    " & & & \\multicolumn{2}{c}{Congruent} & \\multicolumn{2}{c}{Incongruent} \\\\", 
+    "\\cmidrule(rl){4-5}",
+    "\\cmidrule(rl){6-7}",
+    "\\multicolumn{1}{c}{Task-Relevant Context} & \\multicolumn{1}{c}{Condition} & \\multicolumn{1}{c}{PC} & \\multicolumn{1}{c}{RT} & \\multicolumn{1}{c}{ER} & \\multicolumn{1}{c}{RT} & \\multicolumn{1}{c}{ER}  \\\\",
+    "\\midrule", 
+    paste0("\\multirow{9}{*}{100\\% PC} & \\multirow{3}{*}{Object} & \\multicolumn{1}{l}{0\\%} & \\multicolumn{1}{l}{", paste(forTable[1,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    paste0("& & \\multicolumn{1}{l}{50\\%} & \\multicolumn{1}{l}{", paste(forTable[2,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    paste0("& & \\multicolumn{1}{l}{100\\%} & \\multicolumn{1}{l}{", paste(forTable[3,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    "\\cmidrule(rl){2-7}",
+    paste0("& \\multirow{3}{*}{Social} & \\multicolumn{1}{l}{0\\%} & \\multicolumn{1}{c}{", paste(forTable[4,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    paste0("& & \\multicolumn{1}{l}{50\\%} & \\multicolumn{1}{l}{", paste(forTable[5,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    paste0("& & \\multicolumn{1}{l}{100\\%} & \\multicolumn{1}{l}{", paste(forTable[6,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    "\\cmidrule(rl){2-7}",
+    paste0("& \\multirow{3}{*}{Social (NR)} & \\multicolumn{1}{l}{0\\%} & \\multicolumn{1}{l}{", paste(forTable[7,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    paste0("& & \\multicolumn{1}{l}{50\\%} & \\multicolumn{1}{l}{", paste(forTable[8,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    paste0("& & \\multicolumn{1}{l}{100\\%} & \\multicolumn{1}{l}{", paste(forTable[9,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    "\\midrule", 
+    
+    paste0("\\multirow{9}{*}{0\\% PC} & \\multirow{3}{*}{Object} & \\multicolumn{1}{l}{0\\%} & \\multicolumn{1}{l}{", paste(forTable[10,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    paste0("& & \\multicolumn{1}{l}{50\\%} & \\multicolumn{1}{l}{", paste(forTable[11,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    paste0("& & \\multicolumn{1}{l}{100\\%} & \\multicolumn{1}{l}{", paste(forTable[12,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    "\\cmidrule(rl){2-7}",
+    paste0("& \\multirow{3}{*}{Social} & \\multicolumn{1}{l}{0\\%} & \\multicolumn{1}{l}{", paste(forTable[13,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    paste0("& & \\multicolumn{1}{l}{50\\%} & \\multicolumn{1}{l}{", paste(forTable[14,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    paste0("& & \\multicolumn{1}{l}{100\\%} & \\multicolumn{1}{l}{", paste(forTable[15,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    "\\cmidrule(rl){2-7}",
+    paste0("& \\multirow{3}{*}{Social (NR)} & \\multicolumn{1}{l}{0\\%} & \\multicolumn{1}{l}{", paste(forTable[16,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    paste0("& & \\multicolumn{1}{l}{50\\%} & \\multicolumn{1}{l}{", paste(forTable[17,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    paste0("& & \\multicolumn{1}{l}{100\\%} & \\multicolumn{1}{l}{", paste(forTable[18,],collapse="} & \\multicolumn{1}{l}{"), "} \\\\"),
+    
+    " & & & & & & \\\\",
+    "\\bottomrule",
+    "\\multicolumn{7}{l}{\\textit{Note}: RT = Reaction Time (ms);  ER = Error Rates (\\%); PC = Proportion Congruent;} \\\\",
+    "\\multicolumn{7}{l}{NR = Non-Repeating; Standard Errors are presented in parantheses.} \\\\",
+    "\\end{tabular}%",
+    "\\end{table}",
+    sep = "\n"
+)
+    
